6d834ed07553c829b3194ab4b5029bc3f1f214b9
chatreezoo <chatreezooseasong@gmail.com>
2025-02-13 10:25:44 +0700

    edit IP

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
 // baseURL:'https://driver-manager-backend.vercel.app'
 // baseURL:'http://192.168.100.108:4000/'
-baseURL:'http://localhost:4000/'
+baseURL:'http://192.168.100.108:3001/'
 
 })
 
